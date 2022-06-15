d6b34c88c652f4d3cec23beb6473dfb503177028
tjsdn <tjsdn@DESKTOP-2SDII0G>
2022-06-15 17:47:24 +0900

    멤버등록 서블릿, 로그아웃서블릿, 로그인 페이지 추가

--- GDJ47_kickkick_semi/src/main/java/com/member/model/dao/MemberDao.java
+++ GDJ47_kickkick_semi/src/main/java/com/member/model/dao/MemberDao.java
@@ -7,7 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
-
+import static common.JDBCTemplate.*;
 import com.member.model.vo.Member;
 
 public class MemberDao {
@@ -30,6 +30,25 @@ public class MemberDao {
 	}
 	
 	
+	//멤버 결과받아오기 공용메소드
+	public static Member getMember(ResultSet rs) {
+		Member m = null;
+		try {
+			m = Member.builder()
+					.email(rs.getString("email"))
+					.password(rs.getString("password"))
+					.name(rs.getString("name"))
+					.birthday(rs.getDate("birthday"))
+					.phone(rs.getString("phone"))
+					.address(rs.getString("address"))
+					.gender(rs.getString("gender"))
+					.build();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return m;
+	}
+	
 	//로그인하기
 	public Member LoginMember(Connection conn, String email, String pwd) {
 		PreparedStatement pstmt = null;
@@ -45,27 +64,36 @@ public class MemberDao {
 			}
 		}catch(SQLException e) {
 			e.printStackTrace();
+		}finally {
+			close(pstmt);
+			close(rs);
 		}
 		return m;
 	}
 	
-	public static Member getMember(ResultSet rs) {
-		Member m = null;
+	//회원가입하기
+	public boolean EnrollMember(Connection conn, Member m) {
+		PreparedStatement pstmt = null;
+		boolean flag = false;
+		int result = 0;
 		try {
-		m = Member.builder()
-				.email(rs.getString("email"))
-				.password(rs.getString("password"))
-				.name(rs.getString("name"))
-				.birthday(rs.getDate("birthday"))
-				.phone(rs.getString("phone"))
-				.address(rs.getString("address"))
-				.gender(rs.getString("gender"))
-				.build();
+			pstmt = conn.prepareStatement(prop.getProperty("enrollMember"));
+			pstmt.setString(1, m.getEmail());
+			pstmt.setString(2, m.getPassword());
+			pstmt.setString(3, m.getName());
+			pstmt.setString(4, m.getPhone());
+			pstmt.setString(5, m.getAddress());
+			pstmt.setString(6, m.getGender());
+			result = pstmt.executeUpdate();
+			if(result>0) flag = true;
 		}catch(SQLException e) {
 			e.printStackTrace();
+		}finally {
+			close(pstmt);
 		}
-		return m;
+		return flag;
 	}
+	
 	
 	
 	
